88505e27f4e109f9d28ed4486eabf22b5347acd4
Evelina Dumitrescu <evelina.a.dumitrescu@gmail.com>
2013-05-14 19:07:34 +0300

    integrating client window

--- Talk2Me_login/ClientWindow.xaml.cs
+++ Talk2Me_login/ClientWindow.xaml.cs
@@ -16,29 +16,127 @@ namespace Talk2Me_login
     /// <summary>
     /// Interaction logic for ClientWindow.xaml
     /// </summary>
+    /// 
+
     public partial class ClientWindow : Window
     {
-       public  Users user;
+        public Users user;
+        public List<GroupsFriends> list_gf;
         public ClientWindow()
         {
-           
+            list_gf = new List<GroupsFriends>();
             InitializeComponent();
-           
+
         }
         public void setUser(Users user)
         {
             this.user = user;
-           
+
             hellouser.Text = "Hi, " + user.FirstName;
             comboBox1.Text = user.Status;
+            // MessageBox.Show(user.GroupsFriends);
+            string str = user.GroupsFriends;
+            string delimit = ",";
+            string[] groups = null;
+            Label newlabel;
+            groups = str.Split(delimit.ToCharArray());
+            for (int i = 0; i < groups.Length; i++)
+            {
+                string group = groups[i];
+                delimit = "[] ";
+                string[] friends = group.Split(delimit.ToCharArray());
+                newlabel = new Label();
+                newlabel.Height = 40;
+                newlabel.HorizontalAlignment = HorizontalAlignment.Center;
+                newlabel.Width = 234;
+                newlabel.FontSize = 14;
+                GroupsFriends newgf = new GroupsFriends();
+
+                newlabel.FontWeight = FontWeights.ExtraBold;
+                newlabel.Content = friends[0];
+                newgf.group = friends[0];
+                listBox1.Items.Add(newlabel);
+                for (int k = 1; k < friends.Length; k++)
+                    if (friends[k].CompareTo(" ") != 0 && friends[k].CompareTo("") != 0)
+                    {
+                       // MessageBox.Show("cuc" + friends[k] + "cuc");
+                        newlabel = new Label();
+                        newlabel.Height = 40;
+                        newlabel.HorizontalAlignment = HorizontalAlignment.Center;
+                        newlabel.Width = 234;
+                        // newlabel.FontWeight = FontWeights.ExtraBold;
+                        newlabel.Content = friends[k];
+                        //    newlabel.MouseLeftButtonUp+= new MouseButtonEventArgs(label_MouseLeftButtonUp);
+                        newgf.friends.Add(friends[k]);
+                        listBox1.Items.Add(newlabel);
+                    }
+                list_gf.Add(newgf);
+                //   MessageBox.Show(groups[i]);
+            }
+            //   MessageBox.Show(groups.Length.ToString());
             //Label Height="40" HorizontalAlignment="Center" Width="234" FontWeight="ExtraBold" Content="Friends" Name="friends"
-            Label newlabel = new Label();
-            newlabel.Height = 40;
-            newlabel.HorizontalAlignment = HorizontalAlignment.Center;
-            newlabel.Width = 234;
-            newlabel.FontWeight = FontWeights.ExtraBold;
-            newlabel.Content = "ceva";
-            listBox1.Items.Add(newlabel);
+
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                listBox1.Items.Clear();
+                List<string> newfr = new List<string>();
+                ListItem li = new ListItem();
+                Label newlabel;
+                for (int i = 0; i < list_gf.Count(); i++)
+                {
+                    newlabel = new Label();
+                    newlabel.Height = 40;
+                    newlabel.HorizontalAlignment = HorizontalAlignment.Center;
+                    newlabel.Width = 234;
+                    newlabel.FontSize = 14;
+
+
+                    newlabel.FontWeight = FontWeights.ExtraBold;
+                    newlabel.Content = list_gf[i];
+
+                    listBox1.Items.Add(newlabel);
+
+                    for (int j = 0; j < list_gf[i].friends.Count(); j++)
+                    {
+                        if (list_gf[i].friends[j].Contains(textBox1.Text))
+                        {
+                            newlabel = new Label();
+                            newlabel.Height = 40;
+                            newlabel.HorizontalAlignment = HorizontalAlignment.Center;
+                            newlabel.Width = 234;
+                            newlabel.FontSize = 14;
+                            newlabel.Content = list_gf[i].friends[j];
+                            listBox1.Items.Add(newlabel);
+                        }
+                    }
+                }
+
+
+            }
+
+        }
+
+        private void listBox1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void label_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+    public class GroupsFriends
+    {
+        public string group;
+        public List<string> friends;
+        public GroupsFriends()
+        {
+            group = "";
+            friends = new List<string>();
         }
     }
 }
